dfdf51f61dff4941164e6aff21af1a5e9ba81a37
WuBiao <aaronbiao@126.com>
2021-07-22 17:06:56 +0800

    chore: 代码压缩、混淆，添加banner
    n

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
-// import typescript from '@rollup/plugin-typescript';
-// import run from '@rollup/plugin-run';
-// import commonjs from '@rollup/plugin-commonjs';
-// import json from '@rollup/plugin-json';
+import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+import { name, version, author, license } from './package.json';
 
 export default {
     input: 'src/index.js',
     output: {
         dir: 'dist',
         format: 'cjs',
-        exports: 'auto'
+        exports: 'auto',
+        banner: `/*
+        * ${name} v${version}
+        * (c) 2021-2022 ${author}
+        * Released under the ${license} License.
+        */`,
     },
-    // plugins: [commonjs()]
+    plugins: [json(), terser({
+        compress: {
+            drop_console: true
+        },
+    })]
 };
