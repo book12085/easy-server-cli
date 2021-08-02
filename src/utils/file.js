cf4ceab3ed35ce6af54915069fafd8b68511bbec
WuBiao <aaronbiao@126.com>
2021-08-02 10:58:00 +0800

    style(file.js): 出去console

--- src/utils/file.js
+++ src/utils/file.js
@@ -53,7 +53,6 @@ const readFile = (filePath, answers) => {
                 fs.writeFileSync(`${answers.name}/${fileName[fileName.length - 1]}`, code);
             }
         } else {
-            console.log(fs.existsSync(`${answers.name}/.eslintrc.json`));
             const pt = prettier.format(code, {
                 parser: 'babel'
             });
